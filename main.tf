432801de1f28e4f59193f18d3d4de4be3c1e7587
saivikas408 <vikas.sai007@gmail.com>
2020-07-26 13:57:20 +0200

    main.tf
    script

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = us-east-2
+}
+
+resource "aws_instance" "myserver" {
+  ami           = "ami-016b213e65284e9c9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
