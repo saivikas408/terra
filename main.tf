eda00d49808f22a863427c9e5f1787a3e8c48950
saivikas408 <vikas.sai007@gmail.com>
2020-07-26 14:23:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region  = var.region
 }
 
+variable "region" {
+  description = "region to deploy terraform"
+}
+
 resource "aws_instance" "myserver" {
   ami           = "ami-016b213e65284e9c9"
   instance_type = "t2.micro"
